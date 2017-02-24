e7f1663d49814e4b3ab6bff6a1a50f94e077df79
Evan-Macbook-pro <bboydart91@gmail.com>
2017-02-24 15:58:19 +0900

    image scrapping logic is added

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log('CONTENT SCRIPT IS LOADED');
+
+function getImages(document) {
+    console.log(document);
+    let images = [];
+    let documentImages = [...document.images];
+    let limit = { width: 50, height: 50 };
+
+    documentImages.forEach((v, i) => {
+        let width = v.clientWidth,
+            height = v.clientHeight;
+        if(width > limit.width && height > limit.height) images.push(v.src);
+    });
+
+    console.log('URL -> ', location.href);
+    return images;
+}
+
+chrome.extension.sendMessage({ // popup.js로 다시 돌려준다
+    action: "getImages",
+    source: getImages(document)
+});
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,6 +1,27 @@
 'use strict';
 
-const a = 'THIS IS TEST';
+chrome.extension.onMessage.addListener((request, sender) => {
+    if (request.action == "getImages") {
+        let images = request.source;
+        console.log(images.length);
+        for(var i = 0; i < images.length; i++) {
+            if(images[i].length < 1) continue;
 
-console.log('\'Allo \'Allo! Popup');
-console.log(a);
+            let _DOM = document.createElement('li');
+            _DOM.style.backgroundImage = "url("+images[i]+")";
+
+            document.getElementById('target').appendChild(_DOM);
+        }
+    }
+});
+
+function onWindowLoad() {
+    chrome.tabs.executeScript(null, { // 현재 실행중인 웹페이지에 스크립트 주입
+        file: "scripts/contentscript.js"
+    }, () => {
+            if (chrome.extension.lastError) {
+                document.body.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
+            }
+        });
+}
+window.onload = onWindowLoad;
